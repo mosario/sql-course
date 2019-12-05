6795192cf3ba25033ee51a1697c3984c132e346a
FrenkYager <Frenk.yager@gmail.com>
2019-12-05 11:17:58 +0400

    eto pizdez

--- task-1/dump.sql
+++ task-1/dump.sql
@@ -1,2 +1,11 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY
+    email text NOT NULL,
+    first_name character varying(255),
+    last_name character varying(255),
+    login text UNIQUE,
+    password text UNIQUE
+);
+
 INSERT INTO users(email, first_name, last_name, login, password) VALUES ('demo@demo.demo', 'pablo', 'picasso', 'demo', 'demo');
 INSERT INTO users(email, first_name, last_name, login, password) VALUES ('admin@demo.demo', 'salvador', 'dali', 'admin', 'admin');
